c2d6948ffd6cc9e6a00d775abc3081a0fd1b6bf4
wroclov <164581539+wroclov@users.noreply.github.com>
2024-12-09 18:31:36 +0100

    generators versus lists performance comparison
    Generators examples with comprehension creation used
    Compared to list usage

---
+++ OOP/generators.py
@@ -0,0 +1,77 @@
+# big diff in performance diff between list and generators
+from memory_profiler import profile
+from collections.abc import Callable
+
+import random
+import time
+
+names = ['Tytus', 'Adrian', 'Max', 'Sylwester', 'Tomasz']
+majors = ['Math', 'Biology', 'Engineering', 'Chemistry', 'Art']
+@profile
+def build_list(num_people: int) -> list:
+    result = []
+    for i in range(num_people):
+        person = {
+                'id' : i,
+                'name': random.choice(names),
+                'major': random.choice(majors)
+                }
+        result.append(person)
+    return result
+
+@profile
+def build_generator(num_people: int) :
+    for i in range(num_people):
+        person = {
+                'id' : i,
+                'name': random.choice(names),
+                'major': random.choice(majors)
+                }
+        yield person
+
+@profile
+def consume_generator(generator):
+    for x in generator:
+        pass
+
+def measure(func, num_people):
+    t1 = time.time()
+    people = func(num_people)
+    t2 = time.time()
+    print(f"{func} executing took {t2-t1} seconds")
+    return people
+
+if __name__ == "__main__":
+    measure(build_list, 1_000_000)
+    gen_people = measure(build_generator, 1_000_000)
+    # in fact mem profiling will be calculated only when real usage of generator is made
+    # generators are not allocating mem until used, they are more efficient than list
+    t1 = time.time()
+    consume_generator(gen_people)
+    t2 = time.time()
+    print(f"{consume_generator} executing took {t2 - t1} seconds")
+
+
+
+    # simplistic examples below
+    print("\n\n Basics are below:")
+    gen_squares =(x*x for x in range(1, 6))
+    print(gen_squares)
+
+    list_squares = [x*x for x in range(1, 6)]
+    print(list_squares)
+    for elem in list_squares:
+        print(elem)
+
+    # conversion to list
+    print(list(gen_squares), "converted to list")
+
+    def square_numbers(nums):
+        for i in nums:
+            yield (i * i)
+
+    my_nums = square_numbers([1,2,3,4,5])
+    print(my_nums)
+    for num in my_nums:
+        print(num)
+
