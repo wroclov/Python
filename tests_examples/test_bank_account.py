e82ff4eb62f8106156429c695fc866d019e546ec
wroclov <164581539+wroclov@users.noreply.github.com>
2025-04-10 00:07:26 +0200

    tests for negative deposit, parametrization, multiple accounts
    Coverage can be checked:
     pytest --cov=bank_account test_bank_account.py
    
    Tests added:
    - with pytest.raises(ValueError) handling
    - pytest.mark.parametrize("amount, expected" [...]) for multiple deposits checks
    - fixture and tests for transfer logic between 2 accounts

--- tests_examples/test_bank_account.py
+++ tests_examples/test_bank_account.py
@@ -19,3 +19,46 @@ def test_withdraw(default_account):
 def test_withdraw_insufficient_funds(default_account):
     with pytest.raises(InsufficientFundsError):
         default_account.withdraw(300)
+
+def test_negative_deposit(default_account):
+    with pytest.raises(ValueError):
+        default_account.deposit(-77)
+
+@pytest.mark.parametrize("amount, expected", [
+    (0, 100),
+    (60, 160),
+    (400, 500),
+])
+def test_multiple_deposits(amount, expected):
+    account = BankAccount("John", 100)
+    account.deposit(amount)
+    assert account.get_balance() == expected
+
+@pytest.fixture
+def alice_account():
+    return BankAccount("Alice", 200)
+
+@pytest.fixture
+def mark_account():
+    return BankAccount("Mark", 100)
+
+def test_transfer_money(alice_account, mark_account):
+    alice_account.transfer_to(mark_account, 50)
+    assert alice_account.get_balance() == 150
+    assert alice_account.get_balance() == 150
+
+def test_transfer_insufficient_funds(mark_account, alice_account):
+    with pytest.raises(InsufficientFundsError):
+        mark_account.transfer_to(alice_account, 200)
+
+def test_transfer_negative_amount(alice_account, mark_account):
+    with pytest.raises(ValueError):
+        alice_account.transfer_to(mark_account, -20)
+
+def test_multiple_account_balances(alice_account, mark_account):
+    charlie_account = BankAccount("Charlie", 300)
+    alice_account.transfer_to(mark_account, 50)
+    mark_account.transfer_to(charlie_account, 75)
+    assert alice_account.get_balance() == 150
+    assert mark_account.get_balance() == 75
+    assert charlie_account.get_balance() == 375
