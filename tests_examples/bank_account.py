98e111c13b3813943aca49517dcb79fc43a153b7
wroclov <164581539+wroclov@users.noreply.github.com>
2025-04-14 00:26:30 +0200

    added logic to raise InsufficientFundsError exception for negative withdraw amount

--- tests_examples/bank_account.py
+++ tests_examples/bank_account.py
@@ -13,8 +13,10 @@ class BankAccount:
         self.balance += amount
 
     def withdraw(self, amount):
+        if amount <= 0:
+            raise ValueError("Withdrawal amount must be positive")
         if amount > self.balance:
-            raise InsufficientFundsError("Insufficient funds to withdraw asked ", amount, "amount")
+            raise InsufficientFundsError("Insufficient funds to withdraw", amount)
         self.balance -= amount
 
     def get_balance(self):
